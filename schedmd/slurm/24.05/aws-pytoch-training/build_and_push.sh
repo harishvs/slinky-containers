cc8b795e4cb04c6c29ca92456cb1fbe5a9ad39d4
Harish Rao <harisvs@amazon.com>
2025-02-08 22:34:48 +0000

    using the gpu dlc now

--- schedmd/slurm/24.05/aws-pytoch-training/build_and_push.sh
+++ schedmd/slurm/24.05/aws-pytoch-training/build_and_push.sh
@@ -1,5 +1,5 @@
-release_version="1.0.0"
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-west-2.amazonaws.com
+release_version="3.0.0"
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-east-1.amazonaws.com
 docker build --target slurmd -t slurmd:24.05.4  .
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/u1m6g1t5
 docker tag slurmd:24.05.4 public.ecr.aws/u1m6g1t5/harish/slinky-slurmd-aws-pytorch-training:$release_version
