fd49020dc5d897b16cb3846c8f4fabfdfaed8e90
Harish Rao <harisvs@amazon.com>
2025-02-09 05:46:35 +0000

    using hugging face dlc

--- schedmd/slurm/24.05/aws-pytoch-training/build_and_push.sh
+++ schedmd/slurm/24.05/aws-pytoch-training/build_and_push.sh
@@ -1,4 +1,4 @@
-release_version="3.0.0"
+release_version="5.0.0"
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-east-1.amazonaws.com
 docker build --target slurmd -t slurmd:24.05.4  .
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/u1m6g1t5
